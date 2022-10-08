2da7cfcb0c54afc715ac42930ece67e963d25da3
kleberkgns <kleber.silva.kgns@gmail.com>
2022-10-07 23:36:45 -0300

    enemy movement

--- Scripts/Enemy.gd
+++ Scripts/Enemy.gd
@@ -4,7 +4,7 @@ extends KinematicBody2D
 # Declare member variables here. Examples:
 var player
 var angle
-
+var speed = 3
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	player = get_parent().get_child(0)
@@ -13,4 +13,4 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	angle = get_angle_to(player.position)
-	move_and_collide(Vector2(1, 0).rotated(angle))
+	move_and_collide(Vector2(1, 0).rotated(angle) * speed)
